035b251377f47cd1beb2dfc9024cbe2630cb3c8d
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-06 07:29:15 -0500

    docs: add address value object code-level documentation.

--- src/main/java/com/acme/oop/shared/domain/model/valueobjects/Address.java
+++ src/main/java/com/acme/oop/shared/domain/model/valueobjects/Address.java
@@ -2,6 +2,16 @@ package com.acme.oop.shared.domain.model.valueobjects;
 
 import java.util.Objects;
 
+/**
+ * Represents a physical address.
+ * This value object is immutable and shared across the bounded contexts.
+ * @param street        the address street, it must not be null or blank
+ * @param city          the address city, it must not be null or blank.
+ * @param postalCode    the address postal code, it must not be null or blank.
+ * @param country       the address country, it must not be null or blank.
+ *
+ * @author Open Source Application Development Team
+ */
 public record Address(String street, String city, String postalCode, String country) {
     public Address {
         if (street == null || street.isBlank()) throw new IllegalArgumentException("Street cannot be null or blank");
