9020cb939cfa880dce4f6850783f61942af169fe
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-06 09:49:54 -0500

    feat(main): integrate sales order creation and order details logging in main application.

--- src/main/java/com/acme/oop/Main.java
+++ src/main/java/com/acme/oop/Main.java
@@ -1,7 +1,13 @@
 package com.acme.oop;
 
 import com.acme.oop.crm.domain.model.aggregates.Customer;
+import com.acme.oop.sales.domain.model.aggregates.SalesOrder;
+import com.acme.oop.sales.domain.model.valueobjects.ProductId;
 import com.acme.oop.shared.domain.model.valueobjects.Address;
+import com.acme.oop.shared.domain.model.valueobjects.Money;
+
+import java.math.BigDecimal;
+import java.util.Currency;
 
 public class Main {
     static void main() {
@@ -13,5 +19,13 @@ public class Main {
         System.out.println("Creating a customer");
         Customer customer = new Customer("John Doe", "john.doe@gmail.com", address);
         System.out.println("Customer Contact Info: " + customer.getContactInfo());
+        SalesOrder order = new SalesOrder(customer.getId());
+        Money price = new Money(new BigDecimal("29.99"), Currency.getInstance("USD"));
+        ProductId productId = new ProductId();
+        order.addItem(productId, 2, price);
+        System.out.println("Order ID:" + order.getId() +
+                " Customer ID: " + order.getCustomerId() +
+                " Order Date: " + order.getOrderDate() +
+                " Total Amount: " + order.getTotalAmountAsString());
     }
 }
